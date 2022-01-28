ac3ce1c8335d5df9318562052d6f1c217fdcf93c
guryu <guryu@DESKTOP-F4H0D8S>
2022-01-28 13:29:24 -0700

    Added a print statement

--- src/lab02/Lab02.java
+++ src/lab02/Lab02.java
@@ -16,6 +16,7 @@ public class Lab02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("This is the submission for lab 2");
     }
     
 }
